ed7bcb702cf6de96a86cb535098dea7ab4d28362
Daniel Luiz <danielluizthiessen@gmail.com>
2021-04-15 16:40:45 -0300

    Implementei as mensagens do servidor

---
+++ scripts.js
@@ -0,0 +1,50 @@
+
+var infoNome = { name: "" };
+
+function entrar() {
+    var nomeUsuario = prompt("Escolha um nome de usuário:");
+    infoNome = { name: nomeUsuario };
+    const requisicaoNome = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants', infoNome);
+    requisicaoNome.then(nomeSucesso);
+    requisicaoNome.catch(nomeErro);
+}
+
+entrar();
+
+function nomeErro(erro) {
+    const statusCode = erro.response.status;
+    if(statusCode != 200){
+        alert("Esse nome de usuário já foi escolhido! Tente novamente");
+        entrar();
+    }
+}
+
+function nomeSucesso(sucesso) {
+    alert("Bem-Vindo " + infoNome.name + "!!!");
+}
+
+const conexao = setInterval(manterConexao, 5000);
+
+function manterConexao(){
+        const status = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status', infoNome);
+}
+
+function buscar() {
+    const buscar = axios.get('https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages');
+    buscar.then(atualizar);
+}
+
+function atualizar(conteudo){
+    console.log(conteudo.data[1]);
+    for(let i=0; i<conteudo.data.length; i++){
+        var elemento = document.querySelector(".mensagens");
+        elemento.innerHTML += 
+        `<div class="item ${conteudo.data[i].type}">
+            <h3>${conteudo.data[i].time} ${conteudo.data[i].from} ${conteudo.data[i].text}</h3>
+        </div>`;    
+    }
+   
+}
+
+const atualizador = setInterval(buscar, 3000);
+
